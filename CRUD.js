5aaf0800bf52eb7544eb058c14c1276ef7e73e10
Miguel Daza <madazabran@unicesar.edu.co>
2022-06-02 22:32:21 -0500

    commit

---
+++ CRUD.js
@@ -0,0 +1,136 @@
+const codigo = document.querySelector("#codigo");
+const nombre = document.querySelector("#nombre");
+const nota1 = document.querySelector("#notaTrabajo");
+const nota2 = document.querySelector("#notaParcial");
+const notaD = document.querySelector("#notaFinal");
+const btnGuardar = document.getElementById("guardar");
+const btnConsultar = document.getElementById("consultar");
+const btnActualizar = document.getElementById("actualizar");
+const btnEliminar = document.getElementById("eliminar");
+const tabla = document.getElementById("tabla");
+let notas = []
+codigo.addEventListener("keyup",(e)=>{
+    n=false
+    notas = JSON.parse(localStorage.getItem("notas"))
+    notas.map(nota =>{
+        if(nota.codigo==codigo.value){
+            nombre.value=nota.nombre
+            nota1.value=nota.notaTrabajo
+            nota2.value=nota.notaParcial
+            notaD.value=nota.notaDefinitiva
+            n=true;
+        }
+        else if(n==false){
+            nombre.value=""
+            nota1.value=""
+            nota2.value=""
+            notaD.value=""
+        }
+    })  
+})
+nota1.addEventListener("keyup",(e)=>{
+    notaD.value=Math.round((nota1.value*0.4+nota2.value*0.6)*10)/10
+})
+nota2.addEventListener("keyup",(e)=>{
+    notaD.value=Math.round((nota1.value*0.4+nota2.value*0.6)*10)/10
+})
+btnGuardar.addEventListener("click", (e)=>{
+   notas=JSON.parse(localStorage.getItem("notas"))
+   if (notas==null){
+       notas=[]
+   }
+    if(codigo.value=="" || nombre.value=="" || nota1.value=="" || nota2.value==""){
+        alert("Debe llenar todos los campos antes de guardar")
+    }
+    else{
+        est = {
+            codigo: codigo.value,
+            nombre: nombre.value,
+            notaTrabajo: nota1.value,
+            notaParcial: nota2.value,
+            notaDefinitiva: notaD.value
+        }
+        notas.push(est)
+        localStorage.setItem("notas", JSON.stringify(notas))
+        codigo.value=""
+        nombre.value=""
+        nota1.value=""
+        nota2.value=""
+        notaD.value=""
+        tabla.innerHTML=""
+        alert("Se han guardado las notas con éxito")
+    }
+})
+
+btnConsultar.addEventListener("click", (e)=>{
+    notas = JSON.parse(localStorage.getItem("notas"))
+    console.log(notas)
+    tabla.innerHTML=""
+    if(notas==null || notas==""){
+        alert("No se encuentra ningún estudiante registrado")
+    }else{
+        notas.map(nota =>{
+            tabla.innerHTML+=`<li><b>Cédula:</b> ${nota.codigo}  -  <b>Nombre:</b> ${nota.nombre}  -  <b>Nota Definitiva:</b> ${nota.notaDefinitiva}</li><br>`
+        }) 
+    } 
+})
+
+btnActualizar.addEventListener("click", (e)=>{
+    newnotas=[]
+    if(codigo.value==""){
+        alert("Debe digitar la cédula primero para actualizar")
+    }
+    else{
+        notas = JSON.parse(localStorage.getItem("notas"))
+        notas.map(nota =>{
+            if(nota.codigo==codigo.value){
+                est={
+                    codigo: codigo.value,
+                    nombre: nombre.value,
+                    notaTrabajo: nota1.value,
+                    notaParcial: nota2.value,
+                    notaDefinitiva: notaD.value
+                }
+                newnotas.push(est)
+            }
+            else{
+                newnotas.push(nota)
+            }
+        }) 
+        localStorage.setItem("notas", JSON.stringify(newnotas))
+        codigo.value=""
+        nombre.value=""
+        nota1.value=""
+        nota2.value=""
+        notaD.value=""
+        tabla.innerHTML=""
+        alert("Se ha actualizado con éxito!!")
+    }
+})
+
+btnEliminar.addEventListener("click", (e)=>{
+    newnotas=[]
+    if(codigo.value==""){
+        alert("Debe digitar la cédula primero para eliminar")
+    }
+    else{
+        notas = JSON.parse(localStorage.getItem("notas"))
+        notas.map(nota =>{
+            if(nota.codigo==codigo.value){
+               
+            }
+            else{
+                newnotas.push(nota)
+            }
+        }) 
+        localStorage.setItem("notas", JSON.stringify(newnotas))
+        codigo.value=""
+        nombre.value=""
+        nota1.value=""
+        nota2.value=""
+        notaD.value=""
+        tabla.innerHTML=""
+        alert("Se ha eliminado el estudiante con éxito")
+       
+    }
+})
